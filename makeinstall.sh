839f2944f408454e3a52cfb77d2021774584d237
ag0r4n <agoran@live.fr>
2015-09-24 19:54:05 +0200

    modify code to include parameters

--- makeinstall.sh
+++ makeinstall.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 #
-# Synchro-Seedbox
-# Auteur: Script rsync par 4r3, script d'installation et php par Jedediah
+# Synchro-Seedbox-NG
+# Author: rsync script by 4r3, install script and PHP by Jedediah, modified by ag0r4n
 #
-# Installation :
-# cd /tmp
-# git clone https://github.com/4r3/synchro-seedbox
-# cd synchro-seedbox
-# chmod +x makeinstall.sh
-#./makeinstall.sh
 #
 
-# variables couleurs
 CSI="\033["
 CEND="${CSI}0m"
 CRED="${CSI}1;31m"
@@ -21,94 +14,81 @@ CBLUE="${CSI}1;34m"
 
 clear
 
-# contrôle droits utilisateur
+# check user rights
 if [ $(id -u) -ne 0 ]
 	then
 		echo ""
-		echo -e "${CRED}Ce script doit être exécuté en root.$CEND" 1>&2
+		echo -e "${CRED} This script must be run as root.$CEND" 1>&2
 		echo ""
 		exit 1
 	fi
 
 # logo
 echo ""
-echo -e "${CBLUE}                                Synchro-Seedbox$CEND"
+echo -e "${CBLUE}                                Synchro-Seedbox-NG$CEND"
 echo ""
 echo -e "${CBLUE}
-                                      |          |_)         _|     
-            __ \`__ \   _ \  __ \   _\` |  _ \  _\` | |  _ \   |    __|
-            |   |   | (   | |   | (   |  __/ (   | |  __/   __| |   
-           _|  _|  _|\___/ _|  _|\__,_|\___|\__,_|_|\___|_)_|  _|   
+				  __   __  __   __  
+				__)  __)  |  ) (__| 
+				                __/ 
 
 $CEND"
 echo ""
-echo -e "${CYELLOW}          Ce script va vous permettre d'installer une synchronisation$CEND"
-echo -e "${CYELLOW}            automatique entre votre serveur dédié et votre seedbox.$CEND"
+echo -e "${CYELLOW}          This script is going to install SSNG$CEND"
+echo -e "${CYELLOW}            a tool to SYNC your seedbox and your NAS$CEND"
 echo ""
-echo -e "${CBLUE}        Script rsync par 4r3, script d'installation et php par Jedediah.$CEND"
+echo -e "${CBLUE}        Script rsync par 4r3, script d'installation et php par Jedediah, modifié par ag0r4n$CEND"
 echo -e "${CBLUE}              Gros merci à ex_rat et à la communauté mondedie.fr !$CEND"
 echo ""
 
-#Récupération des informations utilisateur
-echo ""
-echo -e "${CGREEN}Entrer votre nom d'utilisateur:$CEND"
-read USER
-echo ""
 
-echo -e "${CGREEN}Entrer le dossier à surveiller sur le serveur\n(/home/user/torrents/complete/dossier):$CEND"
-read FOLDER
-echo ""
-
-echo -e "${CGREEN}Entrer l'utilisateur SSD du NAS:$CEND"
+echo -e "${CGREEN}Enter SSDG NAS Username:$CEND"
 read NASUSER
 echo ""
 
-echo -e "${CGREEN}Entrer l'adresse de votre NAS:$CEND"
+echo -e "${CGREEN}Enter your NAS hostname/IP address:$CEND"
 read NASADDR
 echo ""
 
-echo -e "${CGREEN}Entrer le dossier de synchro sur le NAS\n(/volumeX/dossier sur NAS Synology):$CEND"
-read NASFOLDER
+echo -e "${CGREEN}Enter your NAS port\n(22):$CEND"
+read NASPORT
 echo ""
 
-echo -e "${CGREEN}Entrer la vitesse de synchronisation souhaitée:$CEND"
+echo -e "${CGREEN}Enter your upload speed limit:$CEND"
 read SPEED
 echo ""
 
-echo -e "${CGREEN}Entrer le répertoire d'installation de la page web\n(/var/www):$CEND"
+echo -e "${CGREEN}Enter the webpage root dir\n(/var/www):$CEND"
 read FOLDERWEB
 echo ""
 
 #Création de l'arborescence du script
-mkdir /home/$USER/synchro
-cp -R script/* /home/$USER/synchro
+mkdir -p /usr/local/etc/synchro-seedbox-ng
+mkdir -p /var/spool/synchro-seedbox-ng
+cp script/ssng /usr/local/bin/
+cp config/synchro.conf /usr/local/etc/synchro-seedbox-ng
 
 #Création de l'arborescence de la page web
-mkdir $FOLDERWEB
-cp -R web/* $FOLDERWEB
+#cp -R web/* $FOLDERWEB
 
 #Ecriture des variables dans le fichier de configuration
-sed -i "s/@user@/$USER/g;" /home/$USER/synchro/config/user.cfg
-sed -i 's#@folder@#'$FOLDER'#' /home/$USER/synchro/config/user.cfg
-sed -i "s/@nasuser@/$NASUSER/g;" /home/$USER/synchro/config/user.cfg
-sed -i "s/@nasaddr@/$NASADDR/g;" /home/$USER/synchro/config/user.cfg
-sed -i 's#@nasfolder@#'$NASFOLDER'#' /home/$USER/synchro/config/user.cfg
-sed -i "s/@speed@/$SPEED/g;" /home/$USER/synchro/config/user.cfg
+sed -i "s/@nasuser@/$NASUSER/g;" /usr/local/etc/synchro-seedbox-ng/synchro.conf
+sed -i "s/@nasaddr@/$NASADDR/g;" /usr/local/etc/synchro-seedbox-ng/synchro.conf
+sed -i "s/@nasport@/$NASPORT/g;" /usr/local/etc/synchro-seedbox-ng/synchro.conf
+sed -i "s/@speed@/$SPEED/g;" /usr/local/etc/synchro-seedbox-ng/synchro.conf
 
-sed -i "s/@user@/$USER/g;" $FOLDERWEB/synchro.php
-
-chmod +x /home/$USER/synchro/synchro.sh
+chmod +x /usr/local/bin/ssng
 
 #write out current crontab
-crontab -l > mycron
+#crontab -l > mycron
 #echo new cron into cron file
-echo "* * * * * cd /home/$USER/synchro && ./synchro.sh > /dev/null" >> mycron
+#echo "* * * * * cd /home/$USER/synchro && ./synchro.sh > /dev/null" >> mycron
 #install new cron file
-crontab mycron
-rm mycron
+#crontab mycron
+#rm mycron
 
 #Suppression des fichiers d'installation
-rm -R /tmp/synchro-seedbox
+#rm -R /tmp/synchro-seedbox
 
-echo "${CBLUE}Merci, vous pouvez maintenant suivre la suite du tutoriel.$CEND"
+echo -e "${CBLUE} Thanks ! $CEND"
 echo ""
